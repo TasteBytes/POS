cb32042a8f8adfa1fe823a75375240dc80b0fea1
Daniel Stansberry <stansberry2295@gmail.com>
2017-04-29 20:43:44 -0700

    support for user authentication routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var userService = require('../user_service');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'POS - Login', styles: ['auth.css']});
+  var signedIn = false;
+  if (userService.firebase.auth().currentUser != null) {
+    res.render('index', { title: 'TatseBytes - POS', styles: ['style.css']});
+
+  } else {
+    res.render('login', { title: 'TatseBytes - POS', styles: ['auth.css']});
+  }
 });
 
 
