a59fa23d3aa9cc4220cd94becef8d469667aa653
Daniel Stansberry <stansberry2295@gmail.com>
2017-05-03 14:12:10 -0700

    minor commit for table and menu routes

--- routes/index.js
+++ routes/index.js
@@ -13,5 +13,16 @@ router.get('/', function(req, res, next) {
   }
 });
 
+/* GET users listing. */
+router.get('/:tableNumber', function(req, res, next) {
+  var signedIn = false;
+  if (userService.firebase.auth().currentUser != null) {
+    res.send('The Table Number is ' + req.params.tableNumber);
+  } else {
+    res.render('login', { title: 'TatseBytes - POS', styles: ['auth.css']});
+  }
+});
+
+
 
 module.exports = router;
