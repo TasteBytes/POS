05f7340e393aa472e8c62251c08737050c588f9a
Daniel Stansberry <stansberry2295@gmail.com>
2017-05-08 11:42:44 -0700

    minor change to formatting

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var userService = require('../user_service');
 
 async function getMenus() {
-    var menus;
-    await userService.firebase.database().ref(`/users/${userService.firebase.auth().currentUser.uid}/menus`)
-        .once('value')
-        .then((snapshot) => {
-            menus = snapshot.val();
-        })
-    return menus
+  var menus;
+  await userService.firebase.database().ref(`/users/${userService.firebase.auth().currentUser.uid}/menus`)
+    .once('value')
+    .then((snapshot) => {
+      menus = snapshot.val();
+    })
+  return menus
 };
 
 
@@ -17,9 +17,16 @@ async function getMenus() {
 router.get('/', function(req, res, next) {
   var signedIn = false;
   if (userService.firebase.auth().currentUser != null) {
-    res.render('index', { title: 'TasteBytes - POS', styles: ['style.css', 'receipt.css'], javascript: ['index.js', 'timer.js']});
+    res.render('index', {
+      title: 'TasteBytes - POS',
+      styles: ['style.css', 'receipt.css'],
+      javascript: ['index.js', 'timer.js', 'menus.js']
+    });
   } else {
-    res.render('login', { title: 'TasteBytes - POS', styles: ['auth.css']});
+    res.render('login', {
+      title: 'TasteBytes - POS',
+      styles: ['auth.css']
+    });
   }
 });
 
@@ -30,18 +37,21 @@ router.get('/:tableNumber', function(req, res, next) {
   var tableNumber = req.params.tableNumber;
   if (userService.firebase.auth().currentUser != null) {
     getMenus()
-        .then(menus => {
-            res.render('menus', {
-                signedIn: signedIn,
-                menus: menus,
-                tableNumber: tableNumber,
-                title: 'POS - Menus',
-                styles: ['style.css', 'receipt.css'],
-                javascript: ['menus.js']
-            })
+      .then(menus => {
+        res.render('menus', {
+          signedIn: signedIn,
+          menus: menus,
+          tableNumber: tableNumber,
+          title: 'POS - Menus',
+          styles: ['style.css', 'receipt.css'],
+          javascript: ['menus.js']
         })
+      })
   } else {
-    res.render('login', { title: 'TasteBytes - POS', styles: ['auth.css']});
+    res.render('login', {
+      title: 'TasteBytes - POS',
+      styles: ['auth.css']
+    });
   }
 });
 
